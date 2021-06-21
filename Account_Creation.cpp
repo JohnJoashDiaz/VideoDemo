955d343070ace4409bf8f1955481acd50df4dfc5
JohnJoashDiaz <kraken19611@gmail.com>
2021-06-21 22:56:54 +0800

    Removed Comments

--- Account_Creation.cpp
+++ Account_Creation.cpp
@@ -15,6 +15,4 @@ int main()
     //Display Username and Password
     cout<< endl<<"Your Username is: " <<username<< endl;
     cout<< "Your password is: " << password<<endl;
-    cout <<"Thank you";
-    //test Pull 
 }
