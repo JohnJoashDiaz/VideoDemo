2a5648f092809e08d5e4532581477f138d517f56
JoeyJD0927 <joeyjd0820@gmail.com>
2021-06-21 15:41:32 +0800

    original file

---
+++ Account_Creation.cpp
@@ -0,0 +1,20 @@
+#include "Account_Creation.h"
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    string username, password;
+    cout<< "CREATE A ACCOUNT"<<endl<<endl;
+    cout<< "Enter a Username: ";
+    cin>> username;
+    cout<< "Enter Password: ";
+    cin>> password;
+
+    //Display Username and Password
+    cout<< endl<<"Your Username is: " <<username<< endl;
+    cout<< "Your password is: " << password<<endl;
+
+}
